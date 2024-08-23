2a6b92314772afbcea0b5a1d15461c80cb1e74a1
iliachesnokov <ic@stabfi.com>
2024-08-24 02:59:00 +0300

    feat: integrate opentelemetry

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
+mod otel;
+
+use dotenv::dotenv;
 use mongodb::Client;
 use service::stream::StreamServiceImpl;
 use service::token::TokenServiceImpl;
 use std::sync::Arc;
 use tokio::net::TcpListener;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::from_default_env())
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    dotenv().ok();
+
+    init_tracing_opentelemetry::tracing_subscriber_ext::init_subscribers()
+        .expect("Failed to initialize tracing");
 
     tracing::info!("Loading config");
     let config = config::load_config().expect("Failed to load config");
@@ -39,5 +40,8 @@ async fn main() {
         .expect("Failed to bind to port");
 
     tracing::info!("Listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .with_graceful_shutdown(otel::shutdown_signal())
+        .await
+        .unwrap();
 }
