7ca889d9c350a62538ced2b7e070aa73ab73e2d7
iliachesnokov <ic@stabfi.com>
2024-08-24 02:58:28 +0300

    feat: add opentelemetry shutdown signal

---
+++ src/otel.rs
@@ -0,0 +1,37 @@
+pub(crate) async fn shutdown_signal() {
+    use std::sync::mpsc;
+    use std::{thread, time::Duration};
+
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    tracing::warn!("signal received, starting graceful shutdown");
+    let (sender, receiver) = mpsc::channel();
+    let _ = thread::spawn(move || {
+        opentelemetry::global::shutdown_tracer_provider();
+        sender.send(()).ok()
+    });
+    let shutdown_res = receiver.recv_timeout(Duration::from_millis(2_000));
+    if shutdown_res.is_err() {
+        tracing::error!("failed to shutdown OpenTelemetry");
+    }
+}
