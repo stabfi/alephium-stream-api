2e7b142d6dda009cf9b66adc1dcead4b3f37e2da
iliachesnokov <ic@stabfi.com>
2024-09-14 23:38:47 +0300

    feat(route): add tracing instrumentation to `token` route

--- crates/route/src/token.rs
+++ crates/route/src/token.rs
@@ -4,6 +4,7 @@ use axum::extract::State;
 use axum::Json;
 use model::token::Token;
 
+#[tracing::instrument(name = "route::token::get_all", skip(state))]
 pub async fn get_all(State(state): State<AppState>) -> Result<Json<Vec<Token>>, ApiError> {
     let tokens = state.token_service.get_all().await?;
 
