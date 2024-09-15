477cd96b610bfea22b493ce9fabfdce42034d596
Ilia Chesnokov <ic@stabfi.com>
2024-09-15 13:55:55 +0000

    feat(sdk): add `events` group types

---
+++ crates/alephium-sdk/src/types/events.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct EventField {
+    pub r#type: String,
+    pub value: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Event {
+    #[serde(rename(deserialize = "blockHash"))]
+    pub block_hash: String,
+    #[serde(rename(deserialize = "txId"))]
+    pub tx_id: String,
+    #[serde(rename(deserialize = "eventIndex"))]
+    pub event_index: u64,
+    pub fields: Vec<EventField>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ContractEventsResponse {
+    pub events: Vec<Event>,
+    #[serde(rename(deserialize = "nextStart"))]
+    pub next_start: u64,
+}
+
+#[derive(Serialize, Debug)]
+pub struct ContractEventsQuery {
+    pub start: i32,
+    pub limit: Option<i32>,
+    pub group: Option<i32>,
+}
