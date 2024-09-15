6840967c99205da3a4014ed1cc5f5cae989a71b4
Ilia Chesnokov <ic@stabfi.com>
2024-09-15 13:57:27 +0000

    feat(sdk): add general types and export modules

--- crates/alephium-sdk/src/types/mod.rs
+++ crates/alephium-sdk/src/types/mod.rs
@@ -1,4 +1,22 @@
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+
+pub mod contracts;
+pub mod events;
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(untagged)]
+pub enum Value {
+    String(String),
+    Boolean(bool),
+    Array(Arc<Vec<Val>>),
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Val {
+    r#type: String,
+    value: Value,
+}
 
 #[derive(Deserialize, Serialize, Debug)]
 pub(crate) struct ApiErrorResponse {
